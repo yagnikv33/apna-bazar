387e3f7be51263799ef8c27a79d24e692bdbbf8c
Yagnik Vadgama <yagnik.v@itindia.co.in>
2021-06-02 20:38:17 +0530

    EC: testimonials UI Done

--- app/src/main/java/com/yudiz/e_cigarette/main/homemodule/testimonials/TestimonialsAct.kt
+++ app/src/main/java/com/yudiz/e_cigarette/main/homemodule/testimonials/TestimonialsAct.kt
@@ -1,21 +1,30 @@
 package com.yudiz.e_cigarette.main.homemodule.testimonials
 
+import com.yudiz.BR
 import com.yudiz.databinding.ActivityTestimonialsBinding
 import com.yudiz.e_cigarette.Layouts
 import com.yudiz.e_cigarette.main.base.BaseAct
+import com.yudiz.e_cigarette.main.base.rv.BaseRvBindingAdapter
 import com.yudiz.e_cigarette.main.common.ApiRenderState
 import com.yudiz.e_cigarette.main.homemodule.model.HomeVM
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class TestimonialsAct :
     BaseAct<ActivityTestimonialsBinding, HomeVM>(Layouts.activity_testimonials) {
-    override val vm: HomeVM?
-        get() = null
+    override val vm: HomeVM by viewModel()
 
-    override val hasProgress: Boolean
-        get() = false
+    override val hasProgress: Boolean = false
 
     override fun init() {
+        setAdapter()
+    }
 
+    private fun setAdapter() {
+        binding.rvTestimonials.adapter = BaseRvBindingAdapter(
+            Layouts.raw_testimonials,
+            mutableListOf("","","","",""),
+            br = BR.data
+        )
     }
 
     override fun renderState(apiRenderState: ApiRenderState) {
