639c5d74f3b4c307ea1ac5f139d937e9a003a527
MRMD1205 <mayankdama34@gmail.com>
2022-12-12 10:47:44 +0530

    - PlayerId fetch issue

--- app/src/main/java/com/esjayit/apnabazar/App.kt
+++ app/src/main/java/com/esjayit/apnabazar/App.kt
@@ -1,8 +1,11 @@
 package com.esjayit.apnabazar
 
-import android.R.attr.phoneNumber
 import android.content.Context
-import android.content.Intent
+import android.util.Log
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ProcessLifecycleOwner
 import androidx.multidex.MultiDexApplication
 import com.esjayit.apnabazar.helper.util.PrefUtil
 import com.esjayit.apnabazar.helper.util.logE
@@ -13,12 +16,12 @@ import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-
-class App : MultiDexApplication() {
+class App : MultiDexApplication(), LifecycleObserver {
 
     private val prefs by inject<PrefUtil>()
     override fun onCreate() {
         super.onCreate()
+        ProcessLifecycleOwner.get().lifecycle.addObserver(AppLifecycleListener(applicationContext))
         initTasks()
     }
 
@@ -67,5 +70,42 @@ class App : MultiDexApplication() {
             prefs.playerId = it
         }
         OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
+    }
+}
+
+class AppLifecycleListener(val appContext: Context) : MultiDexApplication(),
+    DefaultLifecycleObserver {
+    private val prefs by inject<PrefUtil>()
+
+    override fun onStart(owner: LifecycleOwner) {
+        Log.e("Application status", "onAppForegrounded")
+        OneSignal.initWithContext(appContext)
+        OneSignal.setAppId(AppConstants.App.ONESIGNAL_APP_ID)
+        OneSignal.unsubscribeWhenNotificationsAreDisabled(true)
+//        OneSignal.promptForPushNotifications()
+
+        OneSignal.setNotificationOpenedHandler(NotificationOpenedHandlerOneSignal(this))
+        OneSignal.addSubscriptionObserver { state ->
+            "One signal $state".logE()
+            state?.let {
+                if (!it.from.isSubscribed && it.to.isSubscribed) {
+                    // get player ID
+                    val oneSignalPlayerId = state.to.userId
+                    if (prefs.playerId != oneSignalPlayerId) {
+                        prefs.playerId = oneSignalPlayerId
+                        prefs.pushToken = state.to.pushToken
+                        // call api
+
+                    }
+                    "One signal $state".logE()
+                }
+            }
+        }
+
+        OneSignal.getDeviceState()?.userId?.let {
+            prefs.playerId = it
+        }
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
+        super.onStart(owner)
     }
 }
